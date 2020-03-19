bdfe2b225c9baa12b9d44be06c021012b08107c7
Kyle Decot <kyle@joinroot.com>
2020-03-19 01:05:33 -0400

    Correct Formula

--- app_store_connect_jwt.rb
+++ app_store_connect_jwt.rb
@@ -1,12 +1,12 @@
 class AppStoreConnectJwt < Formula
   version "0.9.0"
-  url "git@github.com:kyledecot/app-store-connect-jwt.git", :using => :git
+  url "git@github.com:kyledecot/app_store_connect_jwt.git", :using => :git
   depends_on "ruby@2.6"
 
   def install
     ENV["GEM_HOME"] = libexec
-    system "gem", "build", "app-store-connect-jwt.gemspec"
-    system "gem", "install", "app-store-connect-jwt-#{version}.gem"
+    system "gem", "build", "app_store_connect_jwt.gemspec"
+    system "gem", "install", "app_store_connect_jwt-#{version}.gem"
     bin.install "exe/app-store-connect-jwt"
     bin.env_script_all_files(libexec/"exe", :GEM_HOME => ENV["GEM_HOME"])
     libexec.install Dir["*"]
